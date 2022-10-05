e2832a63158fe44854717aea050d44869b29ec64
Rafael Waltrick <rafael.waltrick@fatec.sp.gov.br>
2022-10-05 15:39:49 -0300

    Criação do html e ligação com o back-end.

--- jogoteca.py
+++ jogoteca.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask,render_template
+
+class jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
 
 app = Flask(__name__)
 
 @app.route('/inicio')
-def ola():
-    return '<h1>Olá Mundo</h1>'
+def inicio():
+    jogo1 = jogo('Pac-Man', 'Come-Come', 'Atari')
+    jogo2 = jogo('Pitfull', 'Tarzam', 'PC')
+    jogo3 = jogo('MonPatroll', 'Lunar', 'Mini-Game')
+    listaJogos = [jogo1, jogo2, jogo3]
+    return render_template('lista.html', titulo = 'Jogos', jogos = listaJogos)
 
-app.run
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastrarJogo.html', titulo = 'Cadastro de jogos' )
+
+app.run()
